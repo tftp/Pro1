6bec2cde6bee29050cb58ac36fb3f396c89e50c8
OS <goloborodko@inbox.ru>
2020-05-14 17:41:09 +0300

    add validate for emails user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ validates :email, format: { with: /\A\w+@\w+\.[a-z]{2,3}\z/}
+ 
 end
