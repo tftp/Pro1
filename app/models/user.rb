9f58d757c52b5bed9d81bc99b3735a49e1045a2e
OS <goloborodko@inbox.ru>
2020-06-02 22:00:59 +0300

    add devises trackable and confirmable, add letter-opener

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise  :database_authenticatable,
+          :registerable,
+          :recoverable,
+          :rememberable,
+          :validatable,
+          :trackable,
+          :confirmable
 
  validates :email, format: { with: /\A\w+@\w+\.[a-z]{2,3}\z/}
- 
+
 end
